212b90bb0d2e5f09e4ca56d6f3016a044644748d
Fluxware LTD <fluxwareltd@gmail.com>
2024-01-23 13:52:27 +0500

    minor improvements

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -7,21 +7,21 @@ export const recordSchema = z.object({
   amount: z.number(),
   label: z.string().optional(),
   vout: z.number().optional(),
-  confirmations: z.number().min(0),
+  confirmations: z.number().min(0).optional(),
   blockhash: z.string().optional(),
   blockheight: z.number().optional(),
   blockindex: z.number().optional(),
   blocktime: z.number().optional(),
   txid: z.string(),
-  walletconflicts: z.array(z.unknown()),
+  walletconflicts: z.array(z.unknown()).optional(),
   time: z.number().transform((n) => new UTCDate(n * 1000)), // convert time to UTC
-  timereceived: z.number().transform((n) => new Date(n * 1000)), // convert time to UTC
+  timereceived: z.number().optional(), // convert time to UTC
   "bip125-replaceable": z.string().optional(),
   abandoned: z.boolean().optional(),
 });
 export type Record = z.infer<typeof recordSchema>;
 
 export const jsonSchema = z.object({
-  result: z.array(recordSchema),
+  result: z.array(recordSchema).min(1),
 });
 export type Json = z.infer<typeof jsonSchema>;
