6333f678ff8a0ce257f5d5a094abb6fd570df5dd
cknost <clemens@clemensknost.de>
2024-02-17 21:44:41 +0100

    Valid html5

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,21 +1,23 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex w-full flex-col">
-      <header className="flex h-16 w-full flex-row items-center justify-between bg-gray-600 px-4">
+      <div>
+          <header className="flex h-16 w-full flex-row items-center justify-between bg-gray-600 px-4">
         <span className="text-2xl font-bold text-white">
           Tarnkappe - Bitcoind Statistik
         </span>
       </header>
+    <main className="flex w-full flex-col">
       <div>{children}</div>
-      <footer className="w-full p-2 text-center">
-        {new Date().getFullYear()} ©{" "}
-        <a
-          className="text-blue-500 transition-colors hover:text-blue-600"
-          href="https://tarnkappe.info"
-        >
-          Tarnkappe.info
-        </a>
-      </footer>
     </main>
+          <footer className="w-full p-2 text-center">
+              {new Date().getFullYear()} ©{" "}
+              <a
+                  className="text-blue-500 transition-colors hover:text-blue-600"
+                  href="https://tarnkappe.info"
+              >
+                  Tarnkappe.info
+              </a>
+          </footer>
+      </div>
   );
 }
