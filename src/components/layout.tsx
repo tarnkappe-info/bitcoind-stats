a2a8675f45e47f9f597c0a33c28588eec9effceb
cknost <clemens@clemensknost.de>
2024-02-17 21:57:27 +0100

    Tarnkappe Header and footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,18 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
       <div>
+          <div data-nosnippet className="tk-nav-topbar-wrapper">
+              <nav className="tk-nav-topbar-container">
+                  <ul className="nav2">
+                      <li className="menu-item"><a href="https://tarnkappe.info">Home</a></li>
+                      <li className="menu-item"><a href="https://tarnkappe.info/forum/">Forum</a></li>
+                      <li className="menu-item"><a href="https://shop.tarnkappe.info">Shop</a></li>
+                      <li className="menu-item"><a href="https://tarnkappe.info/spenden/">Spenden</a></li>
+                      <li className="menu-item"><a href="https://podcast.tarnkappe.info">Podcast</a></li>
+                  </ul>
+              </nav>
+          </div>
+
           <header className="flex h-16 w-full flex-row items-center justify-between bg-gray-600 px-4">
         <span className="text-2xl font-bold text-white">
           Tarnkappe - Bitcoind Statistik
@@ -9,15 +21,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <main className="flex w-full flex-col">
       <div>{children}</div>
     </main>
-          <footer className="w-full p-2 text-center">
-              {new Date().getFullYear()} ©{" "}
-              <a
-                  className="text-blue-500 transition-colors hover:text-blue-600"
-                  href="https://tarnkappe.info"
-              >
-                  Tarnkappe.info
-              </a>
+
+          <footer data-nosnippet className="footer_tk-bottom">
+              <div>
+                  <p><a href="https://tarnkappe.info/impressum">Impressum</a> <a href="https://tarnkappe.info/datenschutzerklaerung">Datenschutz</a></p>
+                  <p>2014-<span>2024</span> Tarnkappe.info</p>
+                  <a href="https://tarnkappe.info/ueber-tarnkappe/"><img className="logo" loading="lazy" src="https://tarnkappe.info/wp-content/uploads/website-header-no-motto-klein.png" alt="Tarnkappe.info Logo"></img></a>
+              </div>
           </footer>
+
       </div>
   );
 }
